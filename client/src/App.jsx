e15b2d65cd1928631dc2c273517ee631cb54c353
Naman Dwivedi <namanidwivedi1234@gmail.com>
2023-11-10 19:00:55 +0530

    Created Pages and Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import React from "react"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Signin from "./Pages/Signin"
+import Signup from "./Pages/Signup"
+import Profile from "./Pages/Profile"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/about" element={<About/>}/>
+      <Route exact path="/signin" element={<Signin/>}/>
+      <Route exact path="/signup" element={<Signup/>}/>
+      <Route exact path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
